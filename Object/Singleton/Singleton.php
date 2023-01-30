bf8ecb315e5573a8cdbca66f48210c84148ccacb
markeze207 <markezeafu@gmail.com>
2023-01-30 21:50:26 +0300

    change singleton

--- Object/Singleton/Singleton.php
+++ Object/Singleton/Singleton.php
@@ -10,6 +10,13 @@ class Singleton
 
     private static ?Singleton $instance = null;
 
+
+    private function __construct() {
+    }
+    private function __clone() {
+    }
+    private function __wakeup() {
+    }
     public static function getInstance(): self // Singleton
     {
         if(!self::$instance)
